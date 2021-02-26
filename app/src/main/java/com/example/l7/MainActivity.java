86974beeec2afda18539b3538826575a0d9046d3
Ketsuppimakkara <77271158+Ketsuppimakkara@users.noreply.github.com>
2021-02-26 12:01:08 +0200

    L7T1

---
+++ app/src/main/java/com/example/l7/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.l7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    TextInputLayout inputBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inputBox = (TextInputLayout) findViewById(R.id.inputBox);
+        text = (TextView) findViewById(R.id.textView);
+        text.setText("Alkuperäinen tylsä teksti.");
+    }
+    public void buttonPress (View v){
+        System.out.println("Hello World!");
+        text.setText(inputBox.getEditText().getText().toString());
+        text.setText("Hello World!");
+    }
+
+}
